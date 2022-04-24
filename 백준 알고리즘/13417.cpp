733d39fbbc9cb5cff5e2719c6a7c7f97c9dbee42
SeungBeomKim <kimsb7218@naver.com>
2022-04-25 00:35:32 +0900

    stl insert

--- 백준 알고리즘/13417.cpp
+++ 백준 알고리즘/13417.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #include <vector>
 using namespace std;
 int main()
 {
 	int n;
 	cin>>n;
-	for(int i=0;i<n;i++)
+	int m;
+	char a;
+	while(n--)
 	{
-		int t;
-		cin>>t;
-		vector<char>c;
-		for(int j=0;j<t;j++){
-			char alpha;
-			cin>>alpha;
-			c.push_back(alpha);
+		vector<char>v;
+		cin>>m;
+		for(int i=0;i<m;i++){
+			cin>>a;
+			v.push_back(a);
 		}
-		sort(c.begin(),c.end());
-		for(int j=0;j<t;j++){
-			cout<<c[j];
+		sort(v.begin(),v.end());
+		for(int j=0;j<v.size();j++){
+			cout<<v[j];
 		}
 		cout<<'\n';
 	}
 }
-	
-
