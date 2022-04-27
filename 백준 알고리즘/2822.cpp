f514a12365195613c107a0d7f6720f166ba6c390
SeungBeomKim <kimsb7218@naver.com>
2022-04-27 16:59:55 +0900

    정렬 알고리즘

--- 백준 알고리즘/2822.cpp
+++ 백준 알고리즘/2822.cpp
@@ -1,28 +1,28 @@
-#include <iostream> 
-#include <algorithm> 
-using namespace std; 
-class problem 
-{ public : int score; 
-			int num;  
-}; 
-bool cmp(problem a, problem b) 
-{ return a.score > b.score; } 
-int main() { 
-	problem* p = new problem[8]; 
-	int sum = 0; int arr[8];
-	for (int i = 0; i < 8; i++) 
-	{ cin >> p[i].score; 
-		p[i].num = i + 1; 
-	} 
-// 안정정렬 (기존의 문제의 번호와 점수에 대한 순서가 바뀌지 X) 
-stable_sort(p, p + 8, cmp);
-for (int i = 0; i < 5; i++) 
-{ sum += p[i].score; 
-arr[i] = p[i].num; } 
-sort(arr, arr + 5); 
-cout << sum << "\n"; 
-for (int i = 0; i < 5; i++) 
-{ cout << arr[i] << " "; } 
-return 0; }
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
+int main()
+{
+	vector<pair<int,int>> v(8);
+	for(int i=0;i<8;i++){
+		cin>>v[i].first;
+		v[i].second = i+1;
+	}
+	sort(v.begin(),v.end(),greater<pair<int,int>>());
+	int sum=0;
+	int idx[5];
+	for(int i=0;i<5;i++){
+		sum+=v[i].first;
+		idx[i]=v[i].second;
+	}
+	sort(idx,idx+5);
+	cout<<sum<<'\n';
+	for(int i=0;i<5;i++)
+	{
+		cout<<idx[i]<<' ';
+	}
+	
+}
