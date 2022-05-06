f219bf8dded81179ae69a3fabe1b89b663310b07
SeungBeomKim <kimsb7218@naver.com>
2022-05-06 18:11:57 +0900

    정렬 알고리즘

--- 백준 알고리즘/10816.cpp
+++ 백준 알고리즘/10816.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
-int x[500001];
-int y[500001];
-int count[500001];
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	
 	int n;
 	cin>>n;
-	for(int i=0;i<n;i++){
-		cin>>x[i];
+	vector<int> a(n);
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
 	}
+	sort(a.begin(),a.end());
 	int m;
 	cin>>m;
-	for(int j=0;j<m;j++){
-		cin>>y[j];
-	}
-	for(int k=0;k<m;k++){
-		for(int j=0;j<n;j++){
-			if(x[j]==y[k]){
-				++count[k];
-			}
-		}
-	}
 	for(int i=0;i<m;i++){
-		cout<<count[i]<<' ';
+		int num;
+		cin>>num;
+		cout<<upper_bound(a.begin(),a.end(),num)-lower_bound(a.begin(),a.end(),num)<<' ';
+	
 	}
+	//상한: 큰수 중에 첫번째 하한 : 같거나 큰 수 중에 첫번째   
 	cout<<'\n';
 	
 }
