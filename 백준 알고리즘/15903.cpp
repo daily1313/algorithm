767e820cf4a91ac32d667772d8407bbf9d81b844
SeungBeomKim <kimsb7218@naver.com>
2022-05-23 20:38:18 +0900

    Update 15903.cpp

--- 백준 알고리즘/15903.cpp
+++ 백준 알고리즘/15903.cpp
@@ -2,28 +2,26 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int a[1000001];
 int main()
 {
 	int n,m;
 	cin>>n>>m;
 	int num;
-	vector<int>a;
-	vector<int>::iterator vi;
+	vector<unsigned long long>a;
+	vector<unsigned long long>::iterator vi;
 	for(int i=0;i<n;i++)
 	{
 		cin>>num;
 		a.push_back(num);
 	}
-	
 	for(int i=0;i<m;i++)
 	{
 		sort(a.begin(),a.end());
-		unsigned long long sum = a[0] + a[1];
+		long long sum = a[0] + a[1];
 		a[0] = sum;
 		a[1] = sum;
 	}
-	int result=0;
+	long long result=0;
 	for(vi=a.begin();vi!=a.end();vi++)
 	{
 		result+=(*vi);
