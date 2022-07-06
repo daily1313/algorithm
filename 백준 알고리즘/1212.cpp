f1c8d6e63b89259ba5d3b95045514796b18b47af
SeungBeomKim <kimsb7218@naver.com>
2022-07-06 19:45:27 +0900

    문자열 알고리즘

--- 백준 알고리즘/1212.cpp
+++ 백준 알고리즘/1212.cpp
@@ -1,14 +1,39 @@
+#include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main()
-{
-	int n=1,m=1;
-	while(n!=0 && m!=0){
-		cin>>n>>m;
-		if(n==0 && m==0){return 0;
-		}
-		cout<<n+m<<'\n'; 
-	}
-}
+string eight[8] = {"000","001","010","011","100","101","110","111"};
 
+int main(){
+    string s;
+    cin >> s;
+    if(s=="0") cout<<"0";
+    string ans = "";
+	bool start = true;
+	
+    for (int i=0; i<s.length(); i++) 
+    {
+        int n = s[i]-'0';
+        
+        if (start == true && n < 4) 
+        {
+            if (n == 0)
+                continue;
+            else if(n == 1)
+                ans +="1";
+            else if (n == 2) 
+                ans +="10";
+            else if (n == 3)
+                ans +="11";
+                
+            start = false;
+        } 
+        else 
+        {
+            ans += eight[n];
+            start = false;
+        }
+    }
+    cout<<ans;
+}
