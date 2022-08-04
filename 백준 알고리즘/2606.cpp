2ef07519596d71c79c720797a057064421374b6e
SeungBeomKim <kimsb7218@naver.com>
2022-08-04 14:37:00 +0900

    bfs dfs

--- 백준 알고리즘/2606.cpp
+++ 백준 알고리즘/2606.cpp
@@ -1,43 +1,36 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
-#include <queue>
-#define max 101
 using namespace std;
-int n, com;
-int cnt=0;
+#define max 101
+int n, e;
 int graph[max][max];
-void bfs(int node){
-	bool visited[max]= {false};
-	
-	queue<int>myqueue;
-	visited[node]=true;
-	myqueue.push(node);
-	
-	while(!myqueue.empty()){
-		int curr = myqueue.front();
-		myqueue.pop();
-		for(int next = 1; next<=com;++next){
-			if(!visited[next] && graph[curr][next]){
-				visited[next] = true;
-				myqueue.push(next);	
-				cnt++;
+bool visited[max];
+int cnt=0;
+void dfs(int node)
+{
+	visited[node] = true;
+	//방문 노드 출력 
+	for(int next=1;next<=n; next++)
+	{
+		//간선이 있고, 방문여부 확인(방문하지 않았을 때, 탐색진행) 
+		if(!visited[next] && graph[node][next]){
+			dfs(next);
+			cnt++;
 		}
 	}
-	}
-	cout<<cnt;
 }
 int main()
 {
-	cin>>com;
-	cin>>n;
-	memset(graph,0,sizeof(graph));
-	for(int i=0;i<n;i++)
+	cin>>n>>e;
+	memset(visited, 0, sizeof(visited));
+	memset(graph, 0, sizeof(graph));
+	for(int i=0;i<e;i++)
 	{
 		int u,v;
 		cin>>u>>v;
 		graph[u][v]=graph[v][u]=1;
 	}
-	bfs(1);
+	dfs(1);
+	cout<<cnt;
 	return 0;
 }
