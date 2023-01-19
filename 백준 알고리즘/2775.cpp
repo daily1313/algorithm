3468e44a3df27237ef989ce0a82a5bebef9b645c
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-01-19 15:08:45 +0900

    dp algorithm

--- 백준 알고리즘/2775.cpp
+++ 백준 알고리즘/2775.cpp
@@ -6,28 +6,25 @@ int main()
 {
     int t;
     int k, n; // a층 b호
- 
+
     cin >> t;
 
-    for(int i=0;i<=14;i++)
+    for (int i = 0; i <= 14; i++)
     {
         dp[i][0] = 0;
         dp[0][i] = i;
     }
 
-    
-
     while (t--)
     {
-        cin>>k>>n;
+        cin >> k >> n;
         for (int i = 1; i <= k; i++)
         {
-            for(int j = 1;j <= n; j++)
+            for (int j = 1; j <= n; j++)
             {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]; 
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
-        cout<<dp[k][n]<<'\n';
+        cout << dp[k][n] << '\n';
     }
-    
 }
