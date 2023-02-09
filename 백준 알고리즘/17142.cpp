763f1437f2aaf2b21eab7a3cfe3a6864fb07c10e
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-10 00:26:20 +0900

    BFS

--- 백준 알고리즘/17142.cpp
+++ 백준 알고리즘/17142.cpp
@@ -7,107 +7,107 @@
 using namespace std;
 
 vector<pair<int, int>> v;
-int mov[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+int mov[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 int n, m, emptyCount;
 int ans = 987654321;
-bool pick[10];
 int board[51][51];
+bool visited_virus[10] = {false};
 int visited[51][51];
 
-void bfs() 
+void bfs()
 {
-    queue<pair<int,int>> q;
+    queue<pair<int, int>> q;
     memset(visited, -1, sizeof(visited));
 
-    for(int i=0;i<v.size();i++)
+    for (int i = 0; i < v.size(); i++)
     {
-        if(pick[i])
+        if (visited_virus[i])
         {
             q.push(make_pair(v[i].first, v[i].second));
-            visited[v[i].first][v[i].second] = -1;
+            visited[v[i].first][v[i].second] = 0;
         }
     }
     int time = 0;
     int zero_count = 0;
-    while(!q.empty())
+    while (!q.empty())
     {
         int x, y;
-        tie(x, y) = q.front();
+        x = q.front().first;
+        y = q.front().second;
         q.pop();
 
-        for(int i=0;i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
             int r = x + mov[i][0];
             int c = y + mov[i][1];
 
-            if(r>=0 && r<n && c>=0 && c<n && board[r][c] !=1 && visited[r][c]==-1)
+            if (r >= 0 && r < n && c >= 0 && c < n)
             {
-                visited[r][c] = visited[x][y] + 1;
-                if(board[r][c] == 0) 
+                if (board[r][c] != 1 && visited[r][c] == -1)
                 {
-                    zero_count++;
-                    time = visited[r][c];
+
+                    visited[r][c] = visited[x][y] + 1;
+                    if (board[r][c] == 0)
+                    {
+                        zero_count++;
+                        time = visited[r][c];
+                    }
+                    q.push(make_pair(r, c));
                 }
-                q.push(make_pair(r,c));
             }
         }
     }
-
-    if(zero_count == emptyCount)
+    if (zero_count == emptyCount)
     {
         ans = min(ans, time);
         return;
     }
-
 }
 
 void select_virus(int idx, int cnt)
 {
-    if(cnt == m)
+    if (cnt == m)
     {
         bfs();
         return;
-    } 
+    }
     else
     {
-        for(int i= idx; i<v.size();i++) 
+        for (int i = idx; i < v.size(); i++)
         {
-            if(!pick[i])
+            if (!visited_virus[i])
             {
-                pick[i] = true;
-                select_virus(i+1, cnt+1);
-                pick[i] = false;
+                visited_virus[i] = true;
+                select_virus(i + 1, cnt + 1);
+                visited_virus[i] = false;
             }
         }
     }
-
-
 }
 
 int main()
 {
-    int n, m;
-    cin>>n>>m;
-    for(int i=0;i<n;i++)
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-                cin>>board[i][j];
-                if(board[i][j] == 0) emptyCount++;
-                if(board[i][j] == 2) v.push_back(make_pair(i,j));
+            cin >> board[i][j];
+            if (board[i][j] == 0)
+                emptyCount++;
+            if (board[i][j] == 2)
+                v.push_back(make_pair(i, j));
         }
     }
 
-    memset(visited, false, sizeof(visited));
-
     select_virus(0, 0);
-    if(ans == 987654321)
+
+    if (ans == 987654321)
     {
         ans = -1;
     }
-    cout<<ans<<'\n';
-    
+    cout << ans << '\n';
+
     return 0;
-
-
 }
