c9e58b8d8c493f213a833e9abcaa8c3dfc458e58
Seungbeom Kim <88074556+daily1313@users.noreply.github.com>
2023-03-10 14:58:00 +0900

    [level 2] Title: 주식가격, Time: 15.49 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
@@ -1,24 +1,39 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
+//     vector<int> answer;
     
-    for(int i=0;i<prices.size();i++)
-    {
-        int count = 0;
-        for(int j=i+1;j<prices.size();j++)
-        {
-            count++;
-            if(prices[i]>prices[j]) 
-            {
-                break;
-            }
+//     for(int i=0;i<prices.size();i++)
+//     {
+//         int count = 0;
+//         for(int j=i+1;j<prices.size();j++)
+//         {
+//             count++;
+//             if(prices[i]>prices[j]) 
+//             {
+//                 break;
+//             }
+//         }
+//         answer.push_back(count);
+//     }
+    
+    vector<int> answer(prices.size());
+    stack<int> s;
+    int size = prices.size();
+    for(int i=0;i<size;i++){
+        while(!s.empty()&&prices[s.top()]>prices[i]){
+            // 값이 줄어들었다면
+            answer[s.top()] = i-s.top(); // 현재 시간 - 해당 시간
+            s.pop(); 
         }
-        answer.push_back(count);
+        s.push(i);
     }
-    
+    while(!s.empty()){
+        answer[s.top()] = size-s.top()-1; // 종료 시간 - 해당 시간
+        s.pop();
+    }
     return answer;
+
 }
